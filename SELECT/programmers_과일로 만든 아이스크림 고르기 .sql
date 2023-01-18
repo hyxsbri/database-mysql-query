8790feb6c783e9fc771584f6a2b667187c7e0ecf
Hyowon Choi / 최효원 <gydnjs1231@yonsei.ac.kr>
2023-01-18 20:58:55 +0900

    Update and rename programmers_과일로 만든 아이스크림 고르기 .sql to SELECT/programmers_과일로 만든 아이스크림 고르기 .sql

--- SELECT/programmers_과일로 만든 아이스크림 고르기 .sql
+++ SELECT/programmers_과일로 만든 아이스크림 고르기 .sql
@@ -4,6 +4,6 @@ select a.flavor from first_half a
 left join icecream_info b
 on a.flavor = b.flavor
 where a.total_order > 3000 and b.ingredient_type = 'fruit_based'
-order by a.total_order desc
+order by a.total_order desc;
 
     
