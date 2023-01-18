c8a346aafd7fb91d309fbc6eb16587ffff3653ee
Hyowon Choi / 최효원 <gydnjs1231@yonsei.ac.kr>
2023-01-18 21:07:13 +0900

    Update and rename programmers_서울에 위치한 식당목록 출력하기.sql to SELECT/programmers_서울에 위치한 식당목록 출력하기.sql

--- SELECT/programmers_서울에 위치한 식당목록 출력하기.sql
+++ SELECT/programmers_서울에 위치한 식당목록 출력하기.sql
@@ -1,10 +1,5 @@
 
 
-
-
-
-
-
 select a.rest_id, a.rest_name, a.food_type, a.favorites,
 a.address, round(avg(b.review_score), 2) score
 from rest_info a
@@ -12,18 +7,5 @@ inner join rest_review b
 on a.rest_id = b.rest_id
 group by a.rest_id
 having a.address like '서울%'
-order by score desc, a.favorites desc
-
-
-
-
-
-
-
-
-
-
-
-
-
+order by score desc, a.favorites desc;
 
