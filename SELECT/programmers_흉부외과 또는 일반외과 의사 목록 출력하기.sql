b44923f6ea1ed812a547eb26644b093c316c54a8
Hyowon Choi / 최효원 <gydnjs1231@yonsei.ac.kr>
2023-01-18 20:55:15 +0900

    Update and rename programmers_흉부외과 또는 일반외과 의사 목록 출력하기.sql to SELECT/programmers_흉부외과 또는 일반외과 의사 목록 출력하기.sql

--- SELECT/programmers_흉부외과 또는 일반외과 의사 목록 출력하기.sql
+++ SELECT/programmers_흉부외과 또는 일반외과 의사 목록 출력하기.sql
@@ -3,5 +3,5 @@
 select dr_name, dr_id, mcdp_cd, date_format(hire_ymd, '%Y-%m-%d') hire_ymd
 from doctor
 where mcdp_cd = 'CS' or mcdp_cd = 'GS'
-order by hire_ymd desc, dr_name 
+order by hire_ymd desc, dr_name;
 
